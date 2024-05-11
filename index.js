a6b19f56c051405606100612db97f95c72d25f3f
Kaung Khant Ko Ko <59255928+Kaung-Khant-Ko-Ko@users.noreply.github.com>
2024-05-11 21:44:31 +0630

    Add files via upload

--- index.js
+++ index.js
@@ -3,13 +3,31 @@ var item = $("#popup-content");
 item.slideUp(0);
 
 $("#share-icon-container").on("click", function () {
-  if (item.is(":hidden")) {
-    item.slideDown();
-    $("#share-icon-container").addClass("show-popup-bg");
-    $("#share-icon").addClass("show-popup");
+  if ($("body").width() > 900) {
+    if (item.is(":hidden")) {
+      item.css("right", "");
+      item.slideDown();
+      $("#share-icon-container").addClass("show-popup-bg");
+      $("#share-icon").addClass("show-popup");
+    } else {
+      item.slideUp();
+      $("#share-icon-container").removeClass("show-popup-bg");
+      $("#share-icon").removeClass("show-popup");
+    }
   } else {
-    item.slideUp();
-    $("#share-icon-container").removeClass("show-popup-bg");
-    $("#share-icon").removeClass("show-popup");
+    if (item.is(":hidden")) {
+      var position = $("#author-container").width() / 2;
+      item.animate({ width: "toggle", right: position });
+      $("#share-icon-container").addClass("show-popup-bg");
+      $("#share-icon").addClass("show-popup");
+      $("#author-container").addClass("show-popup-author");
+      $("#author-content").addClass("hide-author");
+    } else {
+      item.animate({ width: "toggle", right: position });
+      $("#share-icon-container").removeClass("show-popup-bg");
+      $("#share-icon").removeClass("show-popup");
+      $("#author-container").removeClass("show-popup-author");
+      $("#author-content").removeClass("hide-author");
+    }
   }
 });
